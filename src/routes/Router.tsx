12fd169c6a5f2964ebea5c64707e3f2307708002
Jose-Morcillo-Cascales <morcillopepe1@gmail.com>
2025-10-15 10:44:32 +0200

    Integrate QueryClientProvider: wrap AppRouter in QueryClientProvider to enable React Query for data fetching, with default options for query retries and stale time management.

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,24 +1,38 @@
 // src/routes/Router.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { lazy, Suspense } from "react"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 // import Layout from './Layout';
 
 const ProductsListPage = lazy(() => import("@/pages/ProductsListPage"))
 const ProductDetailPage = lazy(() => import("@/pages/ProductDetailPage"))
 const CartPage = lazy(() => import("@/pages/CartPage"))
 
+//TODO DESPUES DE CREAR EL CONTEXT/PROVIDER DE REDUX METER EN UN GENERAL PROVIDER  LOS 2
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 2, // reintenta fallos puntuales
+      staleTime: 30_000, // 30s de datos “frescos”
+      refetchOnWindowFocus: false, // no refrescar al volver a la pestaña
+    },
+  },
+})
+
 export default function AppRouter() {
   return (
-    <BrowserRouter>
-      <Suspense fallback={<p>Cargando…</p>}>
-        <Routes>
-          {/* <Route path="/" element={<Layout />}> */}
-          <Route index element={<ProductsListPage />} />
-          <Route path="products/:id" element={<ProductDetailPage />} />
-          <Route path="cart" element={<CartPage />} />
-          {/* </Route> */}
-        </Routes>
-      </Suspense>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Suspense fallback={<p>Cargando…</p>}>
+          <Routes>
+            {/* <Route path="/" element={<Layout />}> */}
+            <Route index element={<ProductsListPage />} />
+            <Route path="products/:id" element={<ProductDetailPage />} />
+            <Route path="cart" element={<CartPage />} />
+            {/* </Route> */}
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
