93033714b4ff60b5f58d950cb9b16e8f106236e6
Jose-Morcillo-Cascales <morcillopepe1@gmail.com>
2025-10-15 20:12:38 +0200

    Add Redux Provider to index.tsx: integrate Redux store for state management in the application.

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import { createRoot } from "react-dom/client"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import AppRouter from "./routes/Router"
 // import { GlobalStyle } from './components/style/generalStyle';
-
+import { Provider } from "react-redux"
+import { store } from "@/store"
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -22,9 +23,11 @@ const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      {/* <GlobalStyle /> */}
-      <AppRouter />
-    </QueryClientProvider>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        {/* <GlobalStyle /> */}
+        <AppRouter />
+      </QueryClientProvider>
+    </Provider>
   </React.StrictMode>
 )
