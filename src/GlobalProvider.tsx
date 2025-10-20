a9428e022eaf8c317c3a9a197e05bdfb7b0af270
Jose-Morcillo-Cascales <morcillopepe1@gmail.com>
2025-10-20 16:48:15 +0200

    refactor: Remove unnecessary whitespace and comments in GlobalProvider component for cleaner code

--- src/GlobalProvider.tsx
+++ src/GlobalProvider.tsx
@@ -1,9 +1,7 @@
 import React from "react"
 import { Provider as ReduxProvider } from "react-redux"
 import { store } from "@/store"
-
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-
 import { GridProvider } from "@/styles/grid"
 
 type Props = {
